b0cea720285f445707a04bfab75f27f87d76c5ca
HirokiObata <hiroki_obata@mail.toyota.co.jp>
2024-05-27 14:18:06 +0900

    viteconfigテスト

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,11 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": "http://localhost:8080",
+      // "/api": process.env.PORT
+      "/api": "https://btc6-solo-banchan.onrender.com",
+      //  "http://localhost:8080"
     },
   },
 });
